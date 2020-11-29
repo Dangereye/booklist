e291d237cccd4afd357b20ef70f61b82510a9617
Dangereye <craig_puxty@talktalk.net>
2020-11-29 12:25:03 +0000

    Boilerplate

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="container">
+                <a className="logo">Booklist</a>
+                <ul>
+                    <li>link</li>
+                    <li>link</li>
+                    <li>link</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
