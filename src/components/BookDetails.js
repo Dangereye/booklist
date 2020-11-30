c1586eea135cfdadbb1aacd51e060af731d76690
Dangereye <craig_puxty@talktalk.net>
2020-11-30 15:55:46 +0000

    Added local storage and updated Booklist with state and styles

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,17 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { BookContext } from '../contexts/BookContext'
 
 const BookDetails = ({ book }) => {
     const { dispatch } = useContext(BookContext)
-
+    const [selected, setSelected] = useState(false)
     return (
-        <li className='book' onClick={() => dispatch({ type: 'REMOVE_BOOK', id: book.id })}>
-            <div className="book-highlight"></div>
+        <li className={selected ? 'book active' : 'book'} onClick={() => setSelected(!selected)}>
             <div className="book-info">
                 <p className='book-title'>{book.title}</p>
                 <p className='book-author'>By {book.author}</p>
+                <div className='book-delete' onClick={() => dispatch({ type: 'REMOVE_BOOK', id: book.id })}>
+                    <i className="fas fa-trash-alt"></i>
+                </div>
             </div>
-
         </li>
     )
 }
